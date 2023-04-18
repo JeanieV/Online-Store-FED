d91fc7bb35438d63a3eeedaf8a29509b6ad16509
JeanieV <venterjeaniebs@gmail.com>
2023-04-18 21:50:15 +0200

    Update #15
    - JSON data added
    - Iframe Map added in Contact page

---
+++ src/js/contact.js
@@ -0,0 +1,53 @@
+//Submit Button
+let submitButton = document.getElementById("submitButton");
+
+//Form Validation
+function createUser(event) {
+    event.preventDefault();
+
+    let firstname1 = document.getElementById("firstname").value;
+    let lastname1 = document.getElementById("lastname").value;
+    let calldate = document.getElementById("calldate").value;
+    let calltime = document.getElementById("calltime").value;
+    let email1 = document.getElementById("email").value;
+
+    //Validations for when the form is empty and completed
+    if (firstname1 !== "" && lastname1 !== "" && calldate !== "" && calltime !== "" && email1 !== "") {
+        alert("Thank you for completing the Contact Form!");
+
+        // Clear the form inputs after submit
+        document.getElementById("firstname").value = '';
+        document.getElementById("lastname").value = '';
+        document.getElementById("calldate").value = '';
+        document.getElementById("calltime").value = '';
+        document.getElementById("email").value = '';
+    }
+    else if (firstname1 !== "") {
+        if (lastname1 == "") {
+            alert("Kindly enter your Last Name!");
+        } else if (calldate == "") {
+            alert("Kindly enter a date we can call you!");
+        } else if (calltime == "") {
+            alert("Kindly enter a suitable time we can call you!");
+        } else if (email1 == "") {
+            alert("Kindly enter your email address");
+        }
+    }
+    else {
+        alert("Please complete your Contact Form ");
+    }
+
+
+
+}
+submitButton.addEventListener("click", createUser);
+
+// Map API
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(51.508742, -0.120850),
+        zoom: 5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
--- src/js/succulents.js
+++ src/js/succulents.js
@@ -1,26 +1,55 @@
-// 'Read more' button
-const readMoreButton1 = document.getElementById('readMore1');
-const closeButton = document.getElementById('closeButton');
+import succulentsData from './succulents.json' assert { type: 'json' };
 
-
-// Function to add a Popup for the details of the product
-function readMore(event) {
-    event.preventDefault();
-    let succulent1R = `
-    <div class="popup1">
-        <img src="/src/images/succulent1.webp">
-        <button class="btn succulentCard" id="closeButton"> Close
-        </button>
-    </div>
-`;
-    document.getElementById("popup").innerHTML += succulent1R;
-}
-readMoreButton1.addEventListener('click', readMore);
-
-// Function to close the popup
-function closeButtonReadMore() {
+class Succulents {
+    constructor(productName,price){
+        this._name = productName;
+        this._price = price;
+    }
     
+    get getproductName(){
+        return this._name;
+    }
+    get getPrice(){
+        return this._price;
+    }
 }
-closeButton.addEventListener('click', closeButtonReadMore);
 
 
+let succulentArray = [];
+
+
+
+
+
+
+
+
+
+
+
+// 'Read more' button
+// const readMoreButton1 = document.getElementById('readMore1');
+// const closeButton = document.getElementsByClassName('closebtn');
+// const popup = document.getElementById('popup');
+
+// function myFunction() {
+
+//     let succulent1R =
+
+//         `
+//     <div class="backgroundpopup">
+//             <img class="succulent1" src="/src/images/succulent1.webp" alt="Succulent Collection"
+//             attribution="https://www.google.com/url?q=https://www.earthandjungle.com/product/tSFF111y/succulent-one-random-potted-plant-bulk-r%3Futm_source%3Dgoogle%26utm_medium%3Dorganic%26utm_content%3Dcatalog&sa=U&ved=0ahUKEwiHnrWcqan-AhXwQkEAHToyDzcQwSsISw&usg=AOvVaw1AFFOR2JhBJkUVVLeWubgU">
+//             <button class="closebtn"> Close Button </button>
+//             </div>
+//     `
+    
+//     document.getElementById('popup').innerHTML += succulent1R;
+    
+// }
+
+// readMoreButton1.addEventListener('click', myFunction);
+
+// closeButton.addEventListener('click', () => {
+//     popup.remove('backgroundpopup');
+// })
