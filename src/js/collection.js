0b5d6f9c6cd101286a902ff22b8db6144df82e06
JeanieV <venterjeaniebs@gmail.com>
2023-04-19 16:25:06 +0200

    Update #17
    Trying to change the products to a class with the modal and not in HTML

--- src/js/collection.js
+++ src/js/collection.js
@@ -4,17 +4,19 @@ let readMoreButton1 = document.getElementById("readMore1");
 let readMoreButton2 = document.getElementById("readMore2");
 let readMoreButton3 = document.getElementById("readMore3");
 let readMoreButton4 = document.getElementById("readMore4");
+let readMoreButton5 = document.getElementById("readMore5");
 let span = document.getElementsByClassName("close")[0];
 let span1 = document.getElementsByClassName("close1")[0];
 let span2 = document.getElementsByClassName("close2")[0];
 let span3 = document.getElementsByClassName("close3")[0];
 let span4 = document.getElementsByClassName("close4")[0];
+let span5 = document.getElementsByClassName("close5")[0];
 let modal = document.getElementById("myModal");
 let modal1 = document.getElementById("myModal1");
 let modal2 = document.getElementById("myModal2");
 let modal3 = document.getElementById("myModal3");
 let modal4 = document.getElementById("myModal4");
-
+let modal5 = document.getElementById("myModal5");
 
 // The modal will open when the user clicks on Read More
 readMoreButton.addEventListener('click', () => {
@@ -32,6 +34,9 @@ readMoreButton3.addEventListener('click', () => {
 readMoreButton4.addEventListener('click', () => {
     modal4.style.display = "block";
 })
+readMoreButton5.addEventListener('click', () => {
+    modal5.style.display = "block";
+})
 
 //Close button
 span.addEventListener('click', () => {
@@ -48,6 +53,9 @@ span3.addEventListener('click', () => {
 })
 span4.addEventListener('click', () => {
     modal4.style.display = "none";
+})
+span5.addEventListener('click', () => {
+    modal5.style.display = "none";
 })
 
 
@@ -77,6 +85,11 @@ window.addEventListener('click', (event) => {
         modal4.style.display = "none";
     }
 })
+window.addEventListener('click', (event) => {
+    if (event.target == modal5) {
+        modal5.style.display = "none";
+    }
+})
 
 // Zoom effect on Images
 
@@ -86,7 +99,7 @@ function imageZoom(modalImage, myResult) {
     let result = document.getElementById(myResult);
 
     /* Create lens: */
-    let lens = document.createElement("DIV");
+    let lens = document.createElement("div");
     lens.setAttribute("class", "img-zoom-lens");
 
     /* Insert lens: */
@@ -160,3 +173,57 @@ imageZoom("modalImage1", "myResult1");
 imageZoom("modalImage2", "myResult2");
 imageZoom("modalImage3", "myResult3");
 imageZoom("modalImage4", "myResult4");
+imageZoom("modalImage5", "myResult5");
+
+
+// class Product {
+//     constructor(name, description, image, price) {
+//       this.name = name;
+//       this.description = description;
+//       this.image = image;
+//       this.price = price;
+//     }
+  
+//     showModal() {
+//       // Get the modal element
+//       const modal = document.getElementById("myModal");
+  
+//       // Get the image and description elements in the modal
+//       const modalImage = modal.querySelector("#modalImage");
+//       const modalDescription = modal.querySelector("#modalDescription");
+  
+//       // Set the values for the modal elements
+//       modalImage.src = this.image;
+//       modalDescription.innerHTML = this.description;
+  
+//       // Display the modal
+//       modal.style.display = "block";
+//     }
+//   }
+  
+//   // Create a new succulent object
+//   const succulent1 = new Product(
+//     "Small Succulent",
+//     "<h3>The Small Succulent plant is made entirely for new garden enthusiasts </h3>" +
+//       "<ul>" +
+//       "<li>Pot Size: 2 to 3 Inch Pot</li>" +
+//       "<li>Plant Size: 2 Inch To 4 Inch plants depending on variety</li>" +
+//       "<li>Keep in direct sun they love sunny weather</li>" +
+//       "<li>Water once in a week if its winter or rainy season</li>" +
+//       "<li>In summer water twice a week</li>" +
+//       "</ul>",
+//     "/src/images/succulent1.webp",
+//     "R25 each"
+//   );
+  
+//   // Get the Read More button and add a click event listener to show the modal
+//   const readMoreBtn = document.getElementById("readMore");
+//   readMoreBtn.addEventListener("click", () => {
+//     succulent1.showModal();
+//   });
+  
+//   // Get the Add to Cart button and add a click event listener to add to cart
+//   const addCartBtn = document.getElementById("addCartBtn");
+//   addCartBtn.addEventListener("click", () => {
+//     // Add to cart logic here
+//   });
--- src/js/succulents.js
+++ src/js/succulents.js
@@ -1,18 +1,158 @@
-import products from './products.json' assert { type: 'json' };
-
-class Products {
-    constructor(productName,price){
-        this._name = productName;
-        this._price = price;
-    }
-    
-    get getproductName(){
-        return this._name;
-    }
-    get getPrice(){
-        return this._price;
-    }
+fetch('/src/js/products.json')
+  .then(response => response.json())
+  .then(data => {
+    const products = {
+    "products" : [
+        {
+            "productName": "Small Succulent",
+            "price": 25,
+            "category": "Succulents"
+        },
+        {
+            "productName": "Mini Succulent Garden",
+            "price": 250,
+            "category": "Succulents"
+        },
+        {
+            "productName": "Succulent in Ceramic Pot",
+            "price": 100,
+            "category": "Succulents"
+        },
+        {
+            "productName": "Seagrass Succulents",
+            "price": 50,
+            "category": "Succulents"
+        },
+        {
+            "productName": "Tin Sinkbad with Succulent Mix",
+            "price": 150,
+            "category": "Succulents"
+        },
+        {
+            "productName": "Gasteria in Bowl",
+            "price": 170,
+            "category": "Succulents"
+        },
+        {
+            "productName": "Mini Glass Succulents",
+            "price": 90,
+            "category": "Succulents"
+        },
+        {
+            "productName": "Firesticks, Pumila & Spekboom Succulents",
+            "price": 90,
+            "category": "Succulents"
+        },
+        {
+            "productName": "Woodbox Succulents",
+            "price": 200,
+            "category": "Succulents"
+        },
+        {
+            "productName": "Circle Handle Potted Succulent",
+            "price": 230,
+            "category": "Succulents"
+        },
+        {
+            "productName": "Large Sedum Pachyphyllum",
+            "price": 180,
+            "category": "Succulents"
+        },
+        {
+            "productName": "Nephrolepis Exaltata Blonde",
+            "price": 130,
+            "category": "Ferns"
+        },
+        {
+            "productName": "Asparagus Plumosus",
+            "price": 90,
+            "category": "Ferns"
+        },
+        {
+            "productName": "Florex Platycerium Staghorn Fern",
+            "price": 110,
+            "category": "Ferns"
+        },
+        {
+            "productName": "Dicksonia Antarctica Fern",
+            "price": 200,
+            "category": "Ferns"
+        },
+        {
+            "productName": "Tuberflora Cyathea Australis Fern",
+            "price": 300,
+            "category": "Ferns"
+        },
+        {
+            "productName": "Large Kangaroo Fern",
+            "price": 380,
+            "category": "Ferns"
+        },
+        {
+            "productName": "Staghorn Fern",
+            "price": 300,
+            "category": "Ferns"
+        },
+        {
+            "productName": "Maiden Hair Fern",
+            "price": 490,
+            "category": "Ferns"
+        },
+        {
+            "productName": "Houseplant Care Gift Set",
+            "price": 650,
+            "category": "Plant Care"
+        },
+        {
+            "productName": "1-For-All Multipurpose Fertiliser 25kg",
+            "price": 730,
+            "category": "Plant Care"
+        },
+        {
+            "productName": "Plant Care - 500 ml",
+            "price": 300,
+            "category": "Plant Care"
+        },
+        {
+            "productName": "Garden Master 60 dm3 Potting Soil",
+            "price": 125,
+            "category": "Plant Care"
+        },
+        {
+            "productName": "Gardening Gloves",
+            "price": 150,
+            "category": "Plant Care"
+        },
+        {
+            "productName": "Dynaroot",
+            "price": 70,
+            "category": "Plant Care"
+        },
+        {
+            "productName": "City Gardening Balcony Box",
+            "price": 850,
+            "category": "Plant Care"
+        }
+    ]
 }
+  
+    
+})
+console.log(data.products);
+
+// class Products {
+//     constructor(productName,price){
+//         this._name = productName;
+//         this._price = price;
+//     }
+    
+//     get getproductName(){
+//         return this._name;
+//     }
+//     get getPrice(){
+//         return this._price;
+//     }
+// }
 
 
-let succulentArray = [];
+// let succulentArray = [];
